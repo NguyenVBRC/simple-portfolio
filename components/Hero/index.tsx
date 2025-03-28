8e728cfffc827b6664fb4658750d3437afc7b0a6
nguyen89mke <nguyen89.mke@gmail.com>
2025-03-27 21:55:57 -0500

    Style Update: HomePage - H1 Font Size and Styled Divs

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -4,10 +4,16 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <Container>
-      <h1 className={styles.heading}>
-        Welcome. <br /> My name is Jimmy Nguyen.
-      </h1>
-      <p>I am a developer building whatever I find interesting.</p>
+      <div className={styles.heroContainer}>
+        <div className={styles.borderLineTopLeft}></div>
+        <div className={styles.borderLineTopRight}></div>
+        <div className={styles.borderLineBottomLeft}></div>
+        <div className={styles.borderLineBottomRight}></div>
+        <h1 className={styles.heading}>
+          Welcome. <br /> My name is Jimmy Nguyen.
+        </h1>
+        <p>I am a developer building whatever I find interesting.</p>
+      </div>
       <div className={styles.verticalLine}></div>
     </Container>
   );
