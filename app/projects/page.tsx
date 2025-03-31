d16ba831efb770b5383e8b0b6a0cd042ff7e1401
nguyen89mke <nguyen89.mke@gmail.com>
2025-03-31 18:20:18 -0500

    Testing ToDo VsCode Extension

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,8 @@
 import PageLayout from "@/components/common/PageLayout";
 import { supabase } from "@/lib/supabase";
 
+// To do: Add preview image to projects
+
 export default async function Page() {
   const heading = "Projects";
   const { data: projects, error } = await supabase.from("Projects").select("*");
